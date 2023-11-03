6ee1f36ba03f43250c4ed69f512233d87533a47c
chromiumsilver <jiaying_yao@berkeley.edu>
2023-11-03 11:45:27 +0800

    feat: Add brighten feature

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-const DEFAULT_COLOR = "#000000";
+const DEFAULT_COLOR = "rgb(0, 0, 0)";
 const DEFAULT_BG_COLOR = "rgb(255, 255, 255)";
 const COLOR_MODE = "color";
 const RAINBOW_MODE = "rainbow";
 const DEFAULT_SQUARES_PER_SIDE = 16;
 const GRADIENT_STEP = Math.floor(255 * 0.1);
+const GRADIENT_OFF = 0;
+const GRADIENT_DARKEN_MODE = -1;
+const GRADIENT_BRIGHTEN_MODE = 1;
 
 let currentColor = DEFAULT_COLOR;
 let currentBgColor = DEFAULT_BG_COLOR;
 let currentMode = COLOR_MODE;
 let currentSquarePerSide = DEFAULT_SQUARES_PER_SIDE;
-// 0: no gradient; 1: brighten; -1: darken
-let gradientMode = 0;
+let gradientMode = GRADIENT_OFF;
 
 const btnSize = document.querySelector("#btn-size");
 const btnColor = document.querySelector("#btn-color");
 const btnRainbow = document.querySelector("#btn-rainbow");
-const btnGradient = document.querySelector("#btn-gradient");
+const btnDarken = document.querySelector("#btn-darken");
+const btnBrighten = document.querySelector("#btn-brighten");
 const btnClear = document.querySelector("#btn-clear");
 
 btnColor.onclick = () => setCurrentMode(COLOR_MODE);
 btnRainbow.onclick = () => setCurrentMode(RAINBOW_MODE);
-btnGradient.onclick = () => setGradientMode(-1);
+btnDarken.onclick = () => setGradientMode(GRADIENT_DARKEN_MODE);
+btnBrighten.onclick = () => setGradientMode(GRADIENT_BRIGHTEN_MODE);
 btnClear.onclick = () => setupGrid();
 
 btnSize.addEventListener("click", () => {
@@ -35,13 +39,16 @@ btnSize.addEventListener("click", () => {
   }
 });
 
-createGrid(currentSquarePerSide);
+setupGrid();
 
-/**
- * Create a grid of squares
- *
- * @param { number } squarePerSide
- */
+function setCurrentMode(mode) {
+  currentMode = mode;
+}
+
+function setGradientMode(mode) {
+  gradientMode = mode;
+}
+
 function createGrid(squarePerSide) {
   const grid = document.querySelector(".grid");
   grid.style.gridTemplateColumns = `repeat(${squarePerSide}, 1fr)`;
@@ -52,25 +59,11 @@ function createGrid(squarePerSide) {
   }
 }
 
-/**
- * Create a single square div
- *
- */
 function createSquare() {
   const square = document.createElement("div");
   square.classList.add("square");
   square.style.backgroundColor = currentBgColor;
-  square.addEventListener("mouseover", () => {
-    if (currentMode === COLOR_MODE) {
-      if (gradientMode) {
-        adjustSquare(square);
-      } else {
-        square.style.backgroundColor = currentColor;
-      }
-    } else if (currentMode === RAINBOW_MODE) {
-      square.style.backgroundColor = getRandomColor();
-    }
-  });
+  square.addEventListener("mouseover", () => changeSquareBgColor(square));
   return square;
 }
 
@@ -93,30 +86,33 @@ function getRandomColor() {
   return color;
 }
 
-function setCurrentMode(mode) {
-  currentMode = mode;
-}
-
-function setGradientMode(mode) {
-  gradientMode = mode;
-}
-
-function adjustSquare(square) {
-  let bgColorRGB = getComputedStyle(square).backgroundColor;
-  console.log(bgColorRGB);
-  if (!bgColorRGB) {
-    bgColorRGB = DEFAULT_BG_COLOR;
+function changeSquareBgColor(square) {
+  let squareBgColor = getComputedStyle(square).backgroundColor;
+  if (currentMode === COLOR_MODE) {
+    if (gradientMode) {
+      squareBgColor = adjustColor(squareBgColor);
+    } else {
+      squareBgColor = currentColor;
+    }
+  } else if (currentMode === RAINBOW_MODE) {
+    squareBgColor = getRandomColor();
   }
-  let { r, g, b } = extractRGB(bgColorRGB);
+  square.style.backgroundColor = squareBgColor;
+}
+
+function adjustColor(color) {
+  if (!color) {
+    color = currentBgColor;
+  }
+  let { r, g, b } = extractRGB(color);
   r = adjustColorChannel(r);
   g = adjustColorChannel(g);
   b = adjustColorChannel(b);
-  square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
 function adjustColorChannel(code) {
   code = code + GRADIENT_STEP * gradientMode;
-
   return pickValidColor(code);
 }
 
